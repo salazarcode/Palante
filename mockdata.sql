a26ceef717009f44e908a4f535aecaed08c1d949
Andrés Salazar <salazarcode@gmail.com>
2020-03-23 11:35:26 -0500

    Nuevo script de mockdata

---
+++ mockdata.sql
@@ -0,0 +1,17 @@
+﻿IF (EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND  TABLE_NAME = 'cliente'))
+BEGIN
+	insert into Cliente values('Andrés Eduardo Salazar Martínez')
+	insert into Cliente values('Sergio Samuel Salazar Martínez')
+	insert into Cliente values('Santiago Isaac Salazar Martínez')
+END
+
+IF (EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND  TABLE_NAME = 'prestamo'))
+BEGIN
+	insert into prestamo values(4515.22, 1)
+	insert into prestamo values(11512.22, 1)
+	insert into prestamo values(965.22, 2)
+	insert into prestamo values(54.22, 2)
+	insert into prestamo values(98651.22, 3)
+	insert into prestamo values(1235.22, 3)
+END
+
